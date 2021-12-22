3c5f351436659478818cf5e9f5058bcb7171663c
AvdhootJadhav <avdhootjadhav002@gmail.com>
2021-12-22 15:45:43 +0530

    Signin and SignUp page

--- AgriChain/main/urls.py
+++ AgriChain/main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.login,name='login'),
     path('index.html', views.home, name='home'),
     path('about.html', views.about, name='about'),
     path('contact.html',views.contact, name='contact'),
